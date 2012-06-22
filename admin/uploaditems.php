0d25328a4b392717f1cdb43737722ec03386a8b3
Ruben <rubenarslan@gmail.com>
2012-06-22 16:37:40 +0200

    more documentation for import

--- admin/uploaditems.php
+++ admin/uploaditems.php
@@ -13,19 +13,35 @@ echo "<table width=\"" . SRVYTBLWIDTH . "\">";
 <tr class="adminmessage"><td>
 Die Datei muss folgenden Kriterien genügen:
 <ol>
-<li>.csv-Datei</li>
-<li>Semicolon (;) als Feldtrenner</li>
-<li>Anführungszeichen (") als Textmarkierungen</li>
+<li>Datei muss entweder .csv, .xls (Excel, aber nicht .xlsx, das neue Format), .ods (OpenOffice), .xml oder .txt sein.</li>
+<li>.csv-Dateien brauchen Komma als Feldtrenner, "" als Feldmarkierung und UTF-8 als Textkodierung. Besser nicht mehr benutzen.</li>
 <li>In der ersten Zeile müssen die Variablennamen stehen (die erste Zeile wird  nicht übernommen!)</li>
-<li>textkodierung muss UTF-8 sein</li>
 <li>genau 14 (mögliche) Multiple-Choice-Alternativen</li>
+<li>Folgende Spalten, in der Reihenfolge: <ul>
+	<li> variablenname</li>
+	<li>wortlaut</li>
+	<li>altwortlautbasedon</li>
+	<li>altwortlaut</li>
+	<li>typ</li>
+	<li>antwortformatanzahl</li>
+	<li>ratinguntererpol</li>
+	<li>ratingobererpol</li>
+	<li>MCalt1, MCalt2,	MCalt3,	MCalt4,	MCalt5,	MCalt6,	MCalt7,	MCalt8,	MCalt9,	MCalt10, MCalt11,	MCalt12,	MCalt13,	MCalt14</li>
+	<li>Teil</li>
+	<li>relevant</li>
+	<li>skipif</li>
+	<li>special</li>
+	<li>rand</li>
+	<li>study</li>
+	</ul>
+</li>
 </ol>
 </td></tr>
 
 <tr class="odd"><td>
 <form enctype="multipart/form-data" action="uploadimport.php" method="POST">
 Bitte Datei auswählen:<br /><input name="uploaded" type="file" /><br /><br />
-<input type="submit" value="Upload & Import" />
+<input type="submit" value="Upload &amp; Import" />
 </form> 
 
 </td></tr>
