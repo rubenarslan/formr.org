bb1be593f2c5134745c6a6743ca1185f388ac3f3
Ruben C. Arslan <rubenarslan@gmail.com>
2013-07-24 12:39:31 +0200

    make some stuff submodules, update some dependencies

--- View/header.php
+++ View/header.php
@@ -10,39 +10,43 @@ header ('Content-type: text/html; charset=utf-8');
 	<?php 
 	if(ONLINE):
 	?>
-		<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.no-icons.min.css" rel="stylesheet">
-		<link href="//netdna.bootstrapcdn.com/font-awesome/3.1.1/css/font-awesome.min.css" rel="stylesheet">
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
-		<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min.js"></script>
-		<!--[if lt IE 9]>
-		<script src="<?=WEBROOT?>js/vendor/html5shiv.min.js"></script>
-		<![endif]-->
-		
+		<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css" rel="stylesheet">
+		<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.min.css" rel="stylesheet">
 	<?php
 	else:
 	?>
-		<link rel="stylesheet" type="text/css" href="<?=WEBROOT?>css/bootstrap.min.css" />
-        <link rel="stylesheet" href="<?=WEBROOT?>css/font-awesome.min.css">
-		<script src="<?=WEBROOT?>js/vendor/jquery-1.9.1.min.js"></script>
-		<script src="<?=WEBROOT?>js/vendor/bootstrap.min.js"></script>
-		<!--[if lt IE 9]>
-		<script src="<?=WEBROOT?>js/vendor/html5shiv.js"></script>
-		<![endif]-->
+		<link rel="stylesheet" type="text/css" href="<?=WEBROOT?>js/vendor/bootstrap/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="<?=WEBROOT?>js/vendor/Font-Awesome/css/font-awesome.min.css">
 	<?php
 	endif;
 	?>
-	    <link rel="stylesheet" href="<?=WEBROOT?>css/bootstrap-responsive.min.css">
-
+	    <link rel="stylesheet" href="<?=WEBROOT?>js/vendor/bootstrap/css/bootstrap-responsive.min.css">
         <!--[if IE 7]>
 		<link rel="stylesheet" href="css/font-awesome-ie7.min.css">
 		<![endif]-->
-		<script src="<?=WEBROOT?>js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
-		<script src="<?=WEBROOT?>js/vendor/js-webshim/minified/polyfiller.js"></script>
-		<script src="<?=WEBROOT?>js/main.js"></script>
 		<link rel="stylesheet" type="text/css" href="<?=WEBROOT?>js/vendor/select2/select2.css">
+		<link rel="stylesheet" href="<?=WEBROOT?>css/main.css" type="text/css" media="screen">
+		
+		
+		<?php 
+		if(ONLINE):
+		?>
+			<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.js"></script>
+			<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
+			<script src="//cdn.jsdelivr.net/webshim/1.10.9/polyfiller.js"></script>
+		<?php
+		else:
+		?>
+			<script src="<?=WEBROOT?>js/vendor/jquery-1.10.2.min.js"></script>
+			<script src="<?=WEBROOT?>js/vendor/bootstrap/js/bootstrap.min.js"></script>
+			<script src="<?=WEBROOT?>js/vendor/webshim/src/polyfiller.js"></script>
+		<?php
+		endif;
+		?>
+		<script src="<?=WEBROOT?>js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+		<script src="<?=WEBROOT?>js/main.js"></script>
 		<script type="text/javascript" src="<?=WEBROOT?>js/vendor/select2/select2.js"></script>
 		
-		<link rel="stylesheet" href="<?=WEBROOT?>css/main.css" type="text/css" media="screen">
 		<?php echo isset($head)?$head:'' ?>
 </head>
 <body>
