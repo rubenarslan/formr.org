99c8f2299ca349f92be8df769f17e4b8cac8a6cf
Ruben C. Arslan <rubenarslan@gmail.com>
2013-07-30 17:58:51 +0200

    various path fixes

--- webroot/admin/survey/index.php
+++ webroot/admin/survey/index.php
@@ -15,7 +15,7 @@ require_once INCLUDE_ROOT.'View/acp_nav.php';
 
 <div class="span8">
 	<h2><?=_('Study settings'); ?></h2>
-	<form id="edit_form" name="edit_form" method="post" action="<?=WEBROOT?>survey/<?=$study->name?>/index" enctype="multipart/form-data" class="form-horizontal">
+	<form id="edit_form" name="edit_form" method="post" action="<?=WEBROOT?>admin/survey/<?=$study->name?>/index" enctype="multipart/form-data" class="form-horizontal">
 	
 <div class="control-group">
 	<p class="control-label">
@@ -25,30 +25,6 @@ require_once INCLUDE_ROOT.'View/acp_nav.php';
 		<input type="file" name="logo" id="logo"/>
 	</p>                    
 </div>
-
-<?php /*
-<div class="control-group">
-	<p class="control-label">
-		<?= _("Require"); ?></label>
-	</p>
-	<p class="controls">
-		<label for="registered"><input type="checkbox" name="registered" id="registered" <?php if($study->registration_required) echo "checked";?>> registration</label><br>
-		<label><input type="checkbox" name="email_required" id="email_required" <?php if($study->email_required) echo "checked";?>>  email</label><br>
-		<label><input type="checkbox" name="birthday_required" id="birthday_required" <?php if($study->birthday_required) echo "checked";?>> a birthday</label>
-	</p>
-</div>
-
-
-<div class="control-group">
-	<p class="control-label">
-		<label for="public"><?= _("Publish this study to the front page"); ?></label>
-	</p>
-	<p class="controls">
-		<input type="checkbox" name="public" id="public" <?php if($study->public) echo "checked"; ?>>
-	</p>
-</div>
-*/	
-	?>
 
 <div class="control-group">
 	<p class="controls">
