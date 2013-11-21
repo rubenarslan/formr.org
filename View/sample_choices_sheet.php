10e7332499d850fb39eba87eb9268fdff6c1b680
Ruben C. Arslan <rubenarslan@gmail.com>
2013-11-21 15:22:41 +0100

    put documentation where needed

---
+++ View/sample_choices_sheet.php
@@ -0,0 +1,75 @@
+
+<h3>Sample choices sheet</h3>
+You can <a href="<?=WEBROOT?>assets/example_surveys/all_widgets.xlsx">download a survey Excel file</a> to get started.
+<table class='table table-striped'>
+	<thead>
+		<tr>
+			<th>
+				list name
+			</th>
+			<th>
+				name
+			</th>
+			<th>
+				label
+			</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>
+				agreement
+			</td>
+			<td>
+				1
+			</td>
+			<td>
+				disagree completely
+			</td>
+		</tr>
+		<tr>
+			<td>
+				agreement
+			</td>
+			<td>
+				2
+			</td>
+			<td>
+				rather disagree
+			</td>
+		</tr>
+		<tr>
+			<td>
+				agreement
+			</td>
+			<td>
+				3
+			</td>
+			<td>
+				neither agree nor disagree
+			</td>
+		</tr>
+		<tr>
+			<td>
+				agreement
+			</td>
+			<td>
+				4
+			</td>
+			<td>
+				rather agree
+			</td>
+		</tr>
+		<tr>
+			<td>
+				agreement
+			</td>
+			<td>
+				5
+			</td>
+			<td>
+				agree completely
+			</td>
+		</tr>
+	</tbody>
+	</table>
