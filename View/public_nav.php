ba7987c86228a0b9760f940ecb492ef5c214170c
Ruben C. Arslan <rubenarslan@gmail.com>
2013-12-03 17:15:24 +0100

    switch to navbars, play with colours and fonts

--- View/public_nav.php
+++ View/public_nav.php
@@ -1,58 +1,72 @@
-<nav class="main_public_nav">
-<ul class="nav nav-tabs">
-    <li <?=endsWith($_SERVER['PHP_SELF'],'index.php')?' class="active"':''?>><a href="<?=WEBROOT?>">
-		<i class="fa fa-circle fa-fw"></i>
-		<?php echo _("home"); ?>
-	</a></li>
-	
-    <li <?=endsWith($_SERVER['PHP_SELF'],'documentation.php')?' class="active"':''?>><a href="<?=WEBROOT?>public/documentation">
-		<i class="fa fa-file fa-fw"></i>
-		<?php echo _("documentation"); ?>
-	</a></li>
+<nav class="main_public_nav navbar navbar-default navbar-formr" role="navigation">
+    <!-- Brand and toggle get grouped for better mobile display -->
+     <div class="navbar-header">
+       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#public-nav-collapse" title="toggle navigaton">
+         <span class="sr-only">Toggle navigation</span>
+         <i class="fa fa-bars"></i>
+       </button>
+       <a class="navbar-brand formr-brand" href="<?=WEBROOT?>"><i class="fa fa-circle fa-fw"></i> formr</a>
+   	
+     </div>
 
-    <li <?=endsWith($_SERVER['PHP_SELF'],'studies.php')?' class="active"':''?>><a href="<?=WEBROOT?>public/studies">
-		<i class="fa fa-pencil-square fa-fw"></i>
-		<?php echo _("studies"); ?>
-	</a></li>
+	  <div class="collapse navbar-collapse" id="public-nav-collapse">
+		<ul class="nav navbar-nav">
+		    <li <?=endsWith($_SERVER['PHP_SELF'],'documentation.php')?' class="active"':''?>><a href="<?=WEBROOT?>public/documentation">
+				<i class="fa fa-file fa-fw"></i>
+				<?php echo _("documentation"); ?>
+			</a></li>
 
-    <li <?=endsWith($_SERVER['PHP_SELF'],'team.php')?' class="active"':''?>><a href="<?=WEBROOT?>public/team">
-		<i class="fa fa-coffee fa-fw"></i>
-		<?php echo _("team"); ?>
-	</a></li>
+		    <li <?=endsWith($_SERVER['PHP_SELF'],'studies.php')?' class="active"':''?>><a href="<?=WEBROOT?>public/studies">
+				<i class="fa fa-pencil-square fa-fw"></i>
+				<?php echo _("studies"); ?>
+			</a></li>
 
-<?php
+		    <li <?=endsWith($_SERVER['PHP_SELF'],'team.php')?' class="active"':''?>><a href="<?=WEBROOT?>public/team">
+				<i class="fa fa-coffee fa-fw"></i>
+				<?php echo _("team"); ?>
+			</a></li>
 
-if($user->isAdmin()):
-   ?>
-    <li><a href="<?=WEBROOT?>admin/">
-		<i class="fa fa-eye-slash fa-fw"></i>
-		<?php echo _("admin control panel"); ?>
-	</a></li>   
-   <?php
-endif;
-if($user->loggedIn()):
-?>
-	<li <?=endsWith($_SERVER['PHP_SELF'],'edit_user.php')?' class="active"':''?>><a href="<?=WEBROOT?>public/edit_user">
-		<i class="fa fa-cogs fa-fw"></i>
-		<?php echo _("settings"); ?></a></li>
-	<li><a href="<?=WEBROOT?>public/logout">
-		<i class="fa fa-cogs fa-sign-out"></i>
-		<?php echo _("logout"); ?></a></li>
-<?php
-else:
-?>
-	<li <?=endsWith($_SERVER['PHP_SELF'],'login.php')?' class="active"':''?>><a href="<?=WEBROOT?>public/login">
-		<i class="fa fa-sign-in fa-fw"></i>
-		<?php echo _("login"); ?>
-	</a></li>
-	<li <?=endsWith($_SERVER['PHP_SELF'],'register.php')?' class="active"':''?>><a href="<?=WEBROOT?>public/register">
-		<i class="fa fa-pencil fa-fw"></i>
-		<?php echo _("sign up") ?>
-	</a></li>
-<?php
-endif;
-?>
-</ul>
+		<?php
+
+		if($user->loggedIn()):
+		?>
+			<li <?=endsWith($_SERVER['PHP_SELF'],'edit_user.php')?' class="active"':''?>><a href="<?=WEBROOT?>public/edit_user">
+				<i class="fa fa-cogs fa-fw"></i>
+				<?php echo _("settings"); ?></a></li>
+			<li><a href="<?=WEBROOT?>public/logout">
+				<i class="fa fa-cogs fa-sign-out"></i>
+				<?php echo _("logout"); ?></a></li>
+		<?php
+		else:
+		?>
+			<li <?=endsWith($_SERVER['PHP_SELF'],'login.php')?' class="active"':''?>><a href="<?=WEBROOT?>public/login">
+				<i class="fa fa-sign-in fa-fw"></i>
+				<?php echo _("login"); ?>
+			</a></li>
+			<li <?=endsWith($_SERVER['PHP_SELF'],'register.php')?' class="active"':''?>><a href="<?=WEBROOT?>public/register">
+				<i class="fa fa-pencil fa-fw"></i>
+				<?php echo _("sign up") ?>
+			</a></li>
+		<?php
+		endif;
+		?>
+		</ul>
+		<ul class="nav navbar-nav navbar-right">
+			<li>
+				<?php
+
+				if($user->isAdmin()):
+				   ?>
+				    <li><a href="<?=WEBROOT?>admin/">
+						<i class="fa fa-eye-slash fa-fw"></i>
+						<?php echo _("go to admin area"); ?>
+					</a></li>   
+				   <?php
+				endif;
+				?>
+			</li>
+		</ul>
+	</div>
 </nav>
 
 <div class="main_body container">
