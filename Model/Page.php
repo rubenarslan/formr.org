1dd225531386abb8bd5a98f8e84e856b376f9dd8
Ruben C. Arslan <rubenarslan@gmail.com>
2014-01-23 09:36:01 +0100

    require parsedown only once

--- Model/Page.php
+++ Model/Page.php
@@ -1,7 +1,7 @@
 <?php
 require_once INCLUDE_ROOT."Model/RunUnit.php";
 #require_once INCLUDE_ROOT. 'vendor/michelf/php-markdown/Michelf/Markdown.php';
-require INCLUDE_ROOT . "vendor/erusev/parsedown/Parsedown.php";
+require_once INCLUDE_ROOT . "vendor/erusev/parsedown/Parsedown.php";
 
 class Page extends RunUnit {
 	public $errors = array();
--- Model/Survey.php
+++ Model/Survey.php
@@ -2,7 +2,7 @@
 require_once INCLUDE_ROOT."Model/DB.php";
 require_once INCLUDE_ROOT."Model/Item.php";
 require_once INCLUDE_ROOT."Model/RunUnit.php";
-require INCLUDE_ROOT . "vendor/erusev/parsedown/Parsedown.php";
+require_once INCLUDE_ROOT . "vendor/erusev/parsedown/Parsedown.php";
 
 class Survey extends RunUnit {
 	public $id = null;
