b7adabfce43a6b2f4c9882a0510154dbb48bca1f
Ruben C. Arslan <rubenarslan@gmail.com>
2014-01-25 14:59:45 +0100

    trying phpunit

---
+++ tests/FirstTest.php
@@ -0,0 +1,43 @@
+<?php
+# phpunit --bootstrap define_root.php tests
+class FirstTest extends PHPUnit_Framework_TestCase
+{
+	private function checkPageForPHPErrors($address)
+	{
+		$bla = file_get_contents($address);
+		$this->assertEquals(FALSE, strpos($bla, "Fatal error") OR strpos($bla, "Parse error") OR strpos($bla, "<b>Warning</b>:") OR strpos($bla, "<b>Notice</b>:") );
+	}
+	public function testAllPages()
+	{
+		$this->checkPageForPHPErrors(WEBROOT);
+		$this->checkPageForPHPErrors(WEBROOT."/public/login");
+		$this->checkPageForPHPErrors(WEBROOT."/public/documentation");
+		$this->checkPageForPHPErrors(WEBROOT."/public/studies");
+		$this->checkPageForPHPErrors(WEBROOT."/public/team");
+		$this->checkPageForPHPErrors(WEBROOT."/public/register");
+		$this->checkPageForPHPErrors(WEBROOT."/public/logout");
+		
+		$this->checkPageForPHPErrors(WEBROOT."/admin/mail/");
+		$this->checkPageForPHPErrors(WEBROOT."/admin/mail/edit");
+		
+		$this->checkPageForPHPErrors(WEBROOT."/admin/run/");
+		
+		$this->checkPageForPHPErrors(WEBROOT."/admin/survey/");
+		
+//		$this->checkPageForPHPErrors(WEBROOT."/admin/cron_log/");
+	}
+# so basically because I rely on a global singleton-ish thing testing is difficult
+#	public function testUploadOfAllWidgetsTable()
+#	{
+#		require_once INCLUDE_ROOT . "Model/Site.php";
+#		require_once INCLUDE_ROOT . "Model/Study.php";
+#		
+#		$study = new Study($fdb, null, array(
+#			'name' => 'all_widgets',
+#			'user_id' => 1
+#		));
+#		$this->assertEquals(TRUE, $study->create() );
+#		$works = $study->uploadItemTable(INCLUDE_ROOT."/webroot/assets/example_surveys/all_widgets.xlsx");
+#		$this->assertEquals(TRUE, $works);
+#	}
+}
