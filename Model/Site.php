bb0f04d3458c30458833a9c925c9f9f2981f945d
Ruben C. Arslan <rubenarslan@gmail.com>
2014-02-05 21:23:58 +0100

    allow longer knitr reports and don't fail hard if they can't be stored in the DB

--- Model/Site.php
+++ Model/Site.php
@@ -457,6 +457,12 @@ function timetostr($timestamp) {
         $age = (int)($age / $factor);
     }
 }
+// from http://de1.php.net/manual/en/function.filesize.php
+function human_filesize($bytes, $decimals = 2) {
+  $sz = 'BKMGTP';
+  $factor = floor((strlen($bytes) - 1) / 3);
+  return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$sz[$factor];
+}
 
 function cr2nl ($string)
 {
