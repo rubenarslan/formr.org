ca89d6b7aa3689b80e3b2f659cfc7b606fbb484f
Ruben C. Arslan <rubenarslan@gmail.com>
2014-02-24 18:54:14 +0100

    fixing massive bug where logged in users were not sent to their login links

--- Model/Site.php
+++ Model/Site.php
@@ -106,7 +106,6 @@ class Site
 		if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity'] > $expiry)) {
 		    // last request was more than 30 minutes ago
 			alert("You were logged out automatically, because you were last active ". timetostr($_SESSION['last_activity']) .'.', 'alert-info');
-			$last_active = $_SESSION['last_activity'];
 		    session_unset();     // unset $_SESSION variable for the run-time 
 		    session_destroy();   // destroy session data in storage
 			session_name("formr_session");
