456e0780e5df156e91fe77daf14d2083e49ec2af
Ruben C. Arslan <rubenarslan@gmail.com>
2014-03-20 12:25:18 +0100

    more work on R package feedback and connect

--- formr-package/R/connect_to_formr.R
+++ formr-package/R/connect_to_formr.R
@@ -35,3 +35,274 @@ formr_import = function(survey_name, host = "https://formr.org") {
 	if(resp$status_code == 200) read.delim(textConnection(httr::content(resp,encoding="utf8",as="text")))
 	else stop("This survey does not exist.")
 }
+
+#' Download items from formr
+#'
+#' After connecting to formr using \code{\link{formr_connect}}
+#' you can download items using this command.
+#'
+#' @param survey_name case-sensitive name of a survey your account owns
+#' @param host defaults to https://formr.org
+#' @export
+#' @examples
+#' \dontrun{
+#' formr_connect(email = "you@@example.net", password = "zebrafinch" )
+#' formr_items(survey_name = "training_diary" )
+#' }
+
+formr_items = function(survey_name, host = "https://formr.org") {
+	resp = httr::GET( paste0(host,"/admin/survey/",survey_name,"/export_item_table?format=json"))
+	if(resp$status_code == 200) jsonlite::fromJSON(
+			httr::content(resp,encoding="utf8",as="text")
+		,simplifyDataFrame = F)
+	else stop("This survey does not exist.")
+}
+
+
+#' Download detailed result timings and display counts from formr
+#'
+#' After connecting to formr using \code{\link{formr_connect}}
+#' you can download detailed times and display counts for each item using this command.
+#'
+#' @param survey_name case-sensitive name of a survey your account owns
+#' @param host defaults to https://formr.org
+#' @export
+#' @examples
+#' \dontrun{
+#' formr_connect(email = "you@@example.net", password = "zebrafinch" )
+#' formr_item_displays(survey_name = "training_diary" )
+#' }
+
+formr_item_displays = function(survey_name, host = "https://formr.org") {
+	resp = httr::GET( paste0(host,"/admin/survey/",survey_name,"/export_itemdisplay?format=json"))
+	if(resp$status_code == 200) as.data.frame(
+		jsonlite::fromJSON(
+			httr::content(resp,encoding="utf8",as="text")
+		))
+	else stop("This survey does not exist.")
+}
+
+#' Download random groups
+#'
+#' formr has a specific module for randomisation.
+#' After connecting using \code{\link{formr_connect}}
+#' you can download the assigned random groups and merge them with your data.
+#'
+#' @param run_name case-sensitive name of the run in which you randomised participants
+#' @param host defaults to https://formr.org
+#' @export
+#' @examples
+#' \dontrun{
+#' formr_connect(email = "you@@example.net", password = "zebrafinch" )
+#' formr_shuffled(run_name = "different_drills" )
+#' }
+
+formr_shuffled = function(run_name, host = "https://formr.org") {
+	resp = httr::GET( paste0(host,"/admin/run/",run_name,"/random_groups_export?format=json"))
+	if(resp$status_code == 200) as.data.frame(
+		jsonlite::fromJSON(
+			httr::content(resp,encoding="utf8",as="text")
+		))
+	else stop("This run does not exist.")
+}
+
+#' Random date in range
+#' 
+#' taken from Dirk Eddelbuettel's answer
+#' here http://stackoverflow.com/a/14721124/263054
+#'
+#' @param N desired number of random dates
+#' @param lower lower limit
+#' @param upper upper limit
+
+random_date_in_range <- function(N, lower="2012/01/01", upper="2012/12/31") {
+	st <- as.POSIXct(as.Date(lower))
+	et <- as.POSIXct(as.Date(upper))
+	dt <- as.numeric(difftime(et,st,units="sec"))
+	ev <- sort(runif(N, 0, dt))
+	rt <- st + ev
+	rt
+}
+
+#' Recognise data types based on item table
+#'
+#' Once you've retrieved an item table using \code{\link{formr_items}} you can use this
+#' function to correctly type your variables based on the item table (e.g. formr free text types will be character, but select_add_one will be factor, dates are also typed as Date, datetimes as POSIXct).
+#'  
+#'
+#' @param survey_name case-sensitive name of a survey your account owns
+#' @param item_list an item_list, will be auto-retrieved based on survey_name if omitted
+#' @param results survey results, will be auto-retrieved based on survey_name if omitted
+#' @param host defaults to https://formr.org
+#' @export
+#' @examples
+#' \dontrun{
+#' formr_connect(email = "you@@example.net", password = "zebrafinch" )
+#' sim = formr_simulate_from_items(item_list = formr_items("training_diary"), n = 100)
+#' summary(lm(pushups ~ pullups, data = sim))
+#' }
+
+formr_recognise = function (survey_name, 
+								item_list = formr_items(survey_name, host = host),
+								results = formr_import(survey_name, host = host),
+								host = "https://formr.org")
+{
+	results$session = as.character(results$session)
+	results$created = as.POSIXct(results$created)
+	results$modified = as.POSIXct(results$modified)
+	results$ended = as.POSIXct(results$ended)
+
+	for(i in seq_along(item_list)) {
+		item = item_list[[i]]
+		if(item$type %in% c("note","mc_heading")) next;
+		if( length( item$choices) )  { # choice-based items
+			choices_type = type.convert( names(item$choices), as.is = F)
+			if(is.character(choices_type)) results[, item$name ] = as.factor(results[, item$name ])
+			else if(is.numeric(choices_type)) results[, item$name ] = as.numeric(results[, item$name ])
+		} else if(item$type %in% c("text","textarea","email","letters")) {
+			results[, item$name ] = as.character(results[, item$name ])
+		} else if(item$type %in% c("datetime")) {
+			results[, item$name ] = as.POSIXct(results[, item$name ])
+		} else if(item$type %in% c("date")) {
+			results[, item$name ] = as.Date(results[, item$name ],format='%Y-%m-%d')
+		} else if(item$type %in% c("time")) {
+#			results[, item$name ] = (results[, item$name ])
+		}
+	}
+	results
+}
+#' Simulate data based on item table
+#'
+#' Once you've retrieved an item table using \code{\link{formr_items}} you can use this
+#' function to sample data from the possible choices.
+#' At the moment random data is only generated for choice-type
+#' items and numeric ones, as these are most likely to enter data analysis.
+#' Does not yet handle dates, times, text, locations, colors
+#'  
+#'
+#' @param item_list the result of a call to \code{\link{formr_connect}}
+#' @param n defaults to 300
+#' @export
+#' @examples
+#' \dontrun{
+#' formr_connect(email = "you@@example.net", password = "zebrafinch" )
+#' sim = formr_simulate_from_items(item_list = formr_items("training_diary"), n = 100)
+#' summary(lm(pushups ~ pullups, data = sim))
+#' }
+
+formr_simulate_from_items = function (item_list, n = 300)
+{
+	sim = data.frame(id = 1:n)
+	sim$created = random_date_in_range(n, Sys.time() - 10000000, Sys.time())
+	sim$ended = sim$created + lubridate::seconds(
+		rpois( n, lambda = length(item_list) * 20) # assume 20 seconds per item
+	)
+	for(i in seq_along(item_list)) {
+		item = item_list[[i]]
+		if(item$type %in% c("note","mc_heading")) next;
+		if( length( item$choices) )  { # choice-based items
+			sample_from = type.convert( names(item$choices), as.is = F)
+			
+			sim[, item$name] = sample(sample_from,size=n,replace=T)
+		} else if(stringr::str_detect(item$type_options, "^[0-9.,]+$")) {
+			limits = as.numeric(
+				stringr::str_split(item$type_options,pattern=stringr::fixed(","))[[1]]
+			)
+			if(length(limits)==3) {
+				sample_from = seq(from = limits[1],to = limits[2], by = limits[3])
+				sim[, item$name] = sample(sample_from,size=n,replace=T)
+			}
+		}
+	}
+	sim
+}
+
+
+#' Aggregate data based on item table
+#'
+#' If you've retrieved an item table using \code{\link{formr_items}} you can use this
+#' function to aggregate your multiple choice items into mean scores. 
+#' If you do not have a item table (e.g. your data was not collected using formr, you don't want another HTTP request in a time-sensitive process).
+#' Example: If your data contains Extraversion_1, Extraversion_2R and Extraversion_3, there will be two new variables in the result: Extraversion_2 (reversed to align with _1 and _2) and Extraversion, the mean score of the three.
+#'  
+#'
+#' @param survey_name case-sensitive name of a survey your account owns
+#' @param item_list an item_list, will be auto-retrieved based on survey_name if omitted
+#' @param results survey results, will be auto-retrieved based on survey_name if omitted
+#' @param host defaults to https://formr.org
+#' @param fallback_max defaults to 5 - if the item_list is set to null, we will use this to reverse items instead
+#' @export
+#' @examples
+#' \dontrun{
+#' formr_connect(email = "you@@example.net", password = "zebrafinch" )
+#' icar_items = formr_items(survey_name="ICAR",host = "http://localhost:8888/formr/")
+#' # get some simulated data and aggregate it
+#' sim_results = formr_simulate_from_items(icar_items)
+#' sim_agg = formr_aggregate(survey_name = "ICAR",item_list = icar_items, results = sim_results)
+#' 
+#' # get actual data
+#' actual = formr_aggregate(survey_name="ICAR")
+#' summary(lm(ICAR_matrix ~ ICAR_verbal, data = sim_agg))
+#' summary(lm(ICAR_matrix ~ ICAR_verbal, data = actual))
+#' }
+
+formr_aggregate = function (survey_name, 
+														item_list = formr_items(survey_name, host = host),
+														results = formr_import(survey_name, host = host),
+														host = "https://formr.org",
+														fallback_max = 5)
+{
+
+	# reverse items
+	if(is.null(item_list)) { # if we're playing dumb and don't have the item table to base our aggregation on?
+		names = names(results) # we use the item names of all items, including notes and text, hoping that there is no false positive
+		reversed_items = stringr::str_detect(names, "^[a-zA-Z0-9_]+?[0-9]+R$") # get reversed items
+		results[,  stringr::str_sub(reversed_items, 1, -2) ] = # reverse these items
+			fallback_max + 1 - results[, reversed_items]				 # based on fallback_max
+	} else {
+		names = character(length(item_list))
+		for(i in seq_along(item_list)) {
+			item = item_list[[i]]
+			if( length( item$choices) )  { # choice-based items
+				if(stringr::str_detect(item$name, "^[a-zA-Z0-9_]+?[0-9]+R$")) {# with a number and an "R" at the end
+					possible_replies = names(item$choices)
+					# save as item name with the R truncated
+					results[, stringr::str_sub(item$name, 1, -2) ] = max(possible_replies) + 1 - results[, item$name ] # reverse
+				}
+			}
+			names[i] = item$name
+		}
+	}
+		
+	scale_stubs = stringr::str_match(names, "^([a-zA-Z0-9_]+?)[0-9]+$")[,2] # fit the pattern
+	scales = unique(na.omit(scale_stubs[duplicated(scale_stubs)])) # only those which occur more than once
+	# todo: should check whether they all share the same reply options (choices, type_options)
+	for(i in seq_along(scales)) {
+		scale = scales[i]
+		# if the scale name ends in an underscore, remove it
+		if(stringr::str_sub(scale, -1) == "_") {
+			save_scale = stringr::str_sub(scale,1, -2)
+		} else
+		{
+			save_scale = scale
+		}
+		
+		if(exists(save_scale,where=results)) {
+			warning(paste("Would have generated scale",save_scale,"but a variable of that name existed already."))
+		} else {
+			scale_item_names = names[which(scale_stubs == scale)]
+			if(! setequal(
+				intersect(scale_item_names, names(results)),
+				scale_item_names)) {
+				warning("Some items were missing. ", paste(setdiff(scale_item_names, names(results)), collapse = " "))
+			} else {
+				results[, save_scale] = rowMeans( results[, scale_item_names ] )
+				cat(paste0("\n\n",save_scale))
+				print(
+					psych::alpha(results[, scale_item_names ], check.keys = F)
+				)
+			}
+		}
+	}
+	results
+}
--- formr-package/R/feedback_plotting.R
+++ formr-package/R/feedback_plotting.R
@@ -1,11 +1,145 @@
-#    qplot_on_normal = function(xintercept,  ylab = "Normal distribution", xlab = '' , colour = "blue") {
-#        z = rnorm(1000)
-#        qplot(z, geom = "blank") +
-#        stat_function(fun=dnorm, aes(x = z)) + 
-#        geom_vline(xintercept= xintercept, colour= colour) +
-#        ylab(ylab) + 
-#        xlab(xlab)
-#    }
+#' Plot a normed value on the standard normal
+#'
+#' Pass in a z-standardised value (x - Mean)/SD,
+#' get a standard normal distribution. 
+#'
+#' @param normed_value a z-standardised value
+#' @param ylab Y-axis label, defaults to "Percentage of other people with this value"
+#' @param xlab X-axis label, empty by default, useful for labeling the plotted trait
+#' @param colour defaults to blue
+#' @param x_ticks the ticks labels for -2,1,0,1 and 2 SDs around the mean, default to minuses, pluses and the average sign 
+#' @export
+#' @import ggplot2
+#' @examples
+#' normed_value = scale(x = 20, center = 14, scale = 5) # standardise value
+#' qplot_on_normal(normed_value, xlab = "Extraversion")
+
+qplot_on_normal = function(normed_value,  ylab = "Percentage of other people with this value", xlab = '' , colour = "blue", x_ticks = c('--','-','0','+','++')) 
+{
+	ggplot()+
+  stat_function(aes(x=-3:3), fun = dnorm,size = I(1)) + 
+  geom_vline(xintercept= normed_value, colour= colour,size = I(1)) +
+	scale_x_continuous(xlab, breaks = c(-2:2),labels = x_ticks) +
+	scale_y_continuous(ylab, labels = scales::percent_format())+
+	theme_minimal() + 
+	theme(text = element_text(size = 18))
+}
+
+
+#' Text feedback based on groups
+#'
+#' If you pass in a z-standardised value (x - Mean)/SD,
+#' and a vector of feedback text chunks, that has either three
+#' or five elements, the text chunks will be used in this order
+#' [very low], low, average, high, [very high] corresponding to these
+#' intervals [low, -2], [-2, -1], [-1, 1], [1, 2], [2, high]
+#'
+#' @param normed_value a z-standardised value
+#' @param chunks a three or five element long character vector containing the text chunks for feedback
+#' @export
+#' @examples
+#' feedback_chunk(normed_value = 0.7, chunks = c("You are rather introverted.","You're approximately as extraverted as most people.","You are rather extraverted."))
+
+feedback_chunk = function(normed_value,  chunks) 
+{
+	chunks = as.character(chunks)
+	if(! (length(chunks) %in% c(3,5))) stop("Have to provide either three or five chunks.")
+	if(length(chunks) == 3) chunks = c(chunks[1], chunks, chunks[3]) # recycle
+	
+	if(normed_value <= -2)      chunks[1]
+	else if(normed_value <= -1) chunks[2]
+	else if(normed_value <= 1)  chunks[3]
+	else if(normed_value <= 2)  chunks[4]
+	else                        chunks[5]
+}
+
+#' Plot normed values as a barchart
+#'
+#' Pass in a data.frame with z-standardised values (x - Mean)/SD,
+#' and variable names, get a bar chart. Getting your data.frame into this shape
+#' probably will mean using reshape2 or dplyr + summarise.
+#' If the data.frame has an se column or ymax/ymin columns, these will be displayed on top of the bars and the bars will become transparent.
+#'
+#' @param normed_data a dataset with a value column containing z-standardised value and a variable column containing labels for those values
+#' @param ylab Y-axis label, defaults to "Percentage of other people with this value"
+#' @param xlab X-axis label, empty by default, useful for labeling the plotted trait
+#' @param title Plot title
+#' @param y_ticks the ticks labels for -2,1,0,1 and 2 SDs around the mean, default to minuses, pluses and the average sign 
+#' @export
+#' @import ggplot2
+#' @examples
+#' normed_data = data.frame(variable = c("Extraversion","Openness","Agreeableness","Neuroticism","Conscientiousness"), value = c(-3,1,-1,0.5,2)) # standardise value
+#' qplot_on_bar(normed_data, title = "Your personality")
+#' normed_data = data.frame(variable = c("Extraversion","Openness","Agreeableness","Neuroticism","Conscientiousness"), value = c(-3,1,-1,0.5,2), se = c(0.2,0.3,0.2,0.25,0.4)) # standardise value
+#' qplot_on_bar(normed_data, title = "Your personality")
+
+qplot_on_bar = function(normed_data, ylab = "Your value", xlab = "Trait", title = '', y_ticks = c('--','-','0','+','++'))
+{
+	if(! c("value","variable") %in% names(normed_data)) stop("Malformed file, check help.")
+	if(exists("se",where = normed_data))
+	{
+		normed_data$ymin = normed_data$value - normed_data$se
+		normed_data$ymax = normed_data$value + normed_data$se
+	}
+	plot = 
+	ggplot(normed_data, aes(x = variable, y = value, fill = variable)) +
+		ggtitle(title)+
+		scale_fill_brewer("",palette="Set1")+
+		scale_y_continuous(ylab, breaks=c(-2,-1,0,1,2),labels= y_ticks) +
+		scale_x_discrete(xlab) +
+		theme_minimal() + 
+		theme(text= element_text(size = 18)) +
+		expand_limits(y=c(-2.5,2.5))
+	if(exists("ymin",where=normed_data)) {
+		plot + geom_linerange(aes(ymin = ymin, ymax = ymax, colour = variable), size = 1) + scale_colour_brewer("",palette="Set1") + geom_bar(stat="identity",position=position_dodge(), alpha = 0.7)
+
+	} else plot + geom_bar(stat="identity",position=position_dodge())
+}
+
+
+#' Time-polar plot
+#'
+#' Pass in a data.frame with z-standardised values (x - Mean)/SD,
+#' and variable names, get a bar chart. Getting your data.frame into this shape
+#' probably will mean using reshape2 or dplyr + summarise.
+#' If the data.frame has an se column or ymax/ymin columns, these will be displayed on top of the bars and the bars will become transparent.
+#'
+#' @param normed_data a dataset with a value column containing z-standardised value and a variable column containing labels for those values
+#' @param ylab Y-axis label, defaults to "Percentage of other people with this value"
+#' @param title Plot title
+#' @export
+#' @import ggplot2
+#' @examples
+#' weekdays = c("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
+#' normed_data = data.frame(variable = factor(weekdays, weekdays), value = c(0,1,0.2,0.5,1.5,2,2)) # standardise value
+#' qplot_on_polar(normed_data, title = "Your alcohol consumption across the week")
+#' normed_data = data.frame(variable = factor(1:24,1:24), value = 3+rnorm(24), se = rep(0.2,24)) # standardise value
+#' qplot_on_polar(normed_data, title = "Your mood around the clock")
+
+qplot_on_polar = function(normed_data, ylab = "Your value", title = '')
+{
+	if(! c("value","variable") %in% names(normed_data)) stop("Malformed file, check help.")
+
+	if(exists("se",where = normed_data))
+	{
+		normed_data$ymin = normed_data$value - normed_data$se
+		normed_data$ymax = normed_data$value + normed_data$se
+	}
+	plot = 
+		ggplot(normed_data, aes(x = variable, y = value, fill = value)) +
+		ggtitle(title)+
+		scale_y_continuous("",breaks=c()) +
+		xlab("") +
+		scale_fill_continuous(ylab) +
+		theme_minimal() + 
+		theme(text= element_text(size = 18)) +
+		coord_polar()
+	if(exists("ymin",where=normed_data)) {
+		plot + geom_linerange(aes(ymin = ymin, ymax = ymax, colour = value), size = 1) + geom_bar(stat="identity",position=position_dodge(), alpha = 0.7)	+ scale_colour_continuous(ylab)
+		
+	} else plot + geom_bar(stat="identity",position=position_dodge())
+}
+
 #    
 #    plot_value_on_other_data = function(variable,title, xlab, ylab = 'Häufigkeit dieses Wertes in unserer Stichprobe',standardize=T, wave2=T) {
 #    tryCatch({
@@ -60,8 +194,8 @@
 #    		scale_alpha_discrete(range = c(0.4, 1))+
 #    		scale_size_discrete(range = c(2, 4))+
 #    		geom_smooth(colour=colo,method="lm",se=F) + 
-#    		scale_x_continuous(xlab,breaks=c(-2,-1,0,1,2),labels=c('--','-','ø','+','++')) + 
-#    		scale_y_continuous(ylab,breaks=c(-2,-1,0,1,2),labels=c('--','-','ø','+','++')) + 
+#    		scale_x_continuous(xlab,breaks=c(-2,-1,0,1,2),labels=c('--','-','0','+','++')) + 
+#    		scale_y_continuous(ylab,breaks=c(-2,-1,0,1,2),labels=c('--','-','0','+','++')) + 
 #    		labs(title=title)
 #    }
 #    
@@ -88,8 +222,8 @@
 #    		geom_smooth(method="lm",colour=wave2colour,se=F) + 
 #    		geom_abline(linetype=2,intercept=meaneff[1] , slope=meaneff[2]) + 
 #    		geom_abline(linetype=2,intercept=meaneff[1] , slope=meaneff[2]) + 
-#    		scale_x_continuous(xlab,breaks=c(-2,-1,0,1,2),labels=c('--','-','ø','+','++')) + 
-#    		scale_y_continuous(ylab,breaks=c(-2,-1,0,1,2),labels=c('--','-','ø','+','++')) +
+#    		scale_x_continuous(xlab,breaks=c(-2,-1,0,1,2),labels=c('--','-','0','+','++')) + 
+#    		scale_y_continuous(ylab,breaks=c(-2,-1,0,1,2),labels=c('--','-','0','+','++')) +
 #    		labs(title=title)
 #    }, error = function(e) {stop(simpleError('Dieser Wert fehlt bei Ihnen.'))})
 #    }
