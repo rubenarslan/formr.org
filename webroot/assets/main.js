5763901b0a4df304a38f357a419867622b832bfd
Ruben C. Arslan <rubenarslan@gmail.com>
2014-03-21 16:34:16 +0100

    some work on the R package and documentation

--- webroot/assets/main.js
+++ webroot/assets/main.js
@@ -39,6 +39,7 @@ $(document).ready(function() {
         $(this).off('click');
     });
     hljs.initHighlighting();
+    $('.nav-tabs').stickyTabs();
 });
 
 
@@ -78,3 +79,35 @@ function ajaxErrorHandling (e, x, settings, exception)
 
 	bootstrap_alert(message, 'Error.','.main_body');
 }
+
+/**
+ * jQuery Plugin: Sticky Tabs
+ *
+ * @author Aidan Lister <aidan@php.net>
+ * @version 1.0.0
+ */
+(function ( $ ) {
+    $.fn.stickyTabs = function() {
+        context = this
+ 
+        // Show the tab corresponding with the hash in the URL, or the first tab.
+        var showTabFromHash = function() {
+          var hash = window.location.hash;
+          var selector = hash ? 'a[href="' + hash + '"]' : 'li:first-child a';
+          $(selector, context).tab('show');
+        }
+ 
+        // Set the correct tab when the page loads
+        showTabFromHash(context)
+ 
+        // Set the correct tab when a user uses their back/forward button
+        window.addEventListener('hashchange', showTabFromHash, false);
+ 
+        // Change the URL when tabs are clicked
+        $('a', context).on('click', function(e) {
+          history.pushState(null, null, this.href);
+        });
+ 
+        return this;
+    };
+}( jQuery ));
