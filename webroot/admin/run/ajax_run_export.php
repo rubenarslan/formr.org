1a35e1ee20f25aad7218fed713c883be08d9d2c0
Cyril Tata <cyril.tata@hotmail.com>
2014-11-09 04:18:24 +0200

    send json file for download after units export

--- webroot/admin/run/ajax_run_export.php
+++ webroot/admin/run/ajax_run_export.php
@@ -8,8 +8,11 @@ if(is_ajax_request() && ($units = $site->request->arr('units')) && ($name = $sit
         bad_request_header();
 		echo $site->renderAlerts();
 	} else {
-		echo 'OK';
+		echo basename($saved);
 	}
+} elseif (($file = $site->request->str('f')) && file_exists(Config::get('run_exports_dir') . '/' . $file)) {
+	// sending file for download
+	download_file(Config::get('run_exports_dir') . '/' . $file, true);
 } else {
 	bad_request_header();
 }
