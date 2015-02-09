11c894b0a19102331890fb2d8b025573ebe0f9ab
Ruben C. Arslan <rubenarslan@gmail.com>
2015-02-09 15:07:55 +0100

    undo merge error

--- View/public/reset_password.php
+++ View/public/reset_password.php
@@ -1,17 +1,13 @@
 <?php Template::load('header_nav'); ?>
-
 <div class="row">
 	<div class="col-lg-4 col-lg-offset-1 col-sm-5 col-sm-offset-1 col-xs-12 well">
 	<h2>Reset password</h2>
 		<form class="" id="login" name="login" method="post" action="<?=WEBROOT?>public/reset_password">
 		<div class="form-group small-left">
 			<label class="control-label sr-only" for="new_password">
+				<?php echo _("New password"); ?>
 	  		  <input required type="hidden" name="email" id="email" value="<?=htmlspecialchars($_GET['email']);?>">
 	  		  <input required type="hidden" name="reset_token" id="reset_token" value="<?=htmlspecialchars($_GET['reset_token']);?>">
-
-
-
-	  		  <input required type="hidden" name="reset_token" id="reset_token" value="<?=htmlspecialchars($user_reset_token);?>">
 				
 			</label>
 			<div class="controls">
