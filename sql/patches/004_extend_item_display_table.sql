92ec606f964246ebc87506b90f4528f62dced3e6
Ruben C. Arslan <rubenarslan@gmail.com>
2015-02-17 16:56:27 +0100

    forgot sth

--- sql/patches/004_extend_item_display_table.sql
+++ sql/patches/004_extend_item_display_table.sql
@@ -2,6 +2,7 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
+ALTER TABLE `survey_items_display` ADD COLUMN `saved` DATETIME NULL;
 UPDATE `survey_items_display` SET `saved` = `answered_time`;
 UPDATE `survey_items_display` SET `answered` = NULL;
 ALTER TABLE `survey_items_display` 
