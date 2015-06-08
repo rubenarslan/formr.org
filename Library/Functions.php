2b63cb00a8ed1a85adf3032fb900b7591761755d
Ruben C. Arslan <rubenarslan@gmail.com>
2015-06-08 15:37:13 +0200

    remove legacy openCPU

--- Library/Functions.php
+++ Library/Functions.php
@@ -18,14 +18,6 @@ function formr_log($msg, $type = '') {// shorthand
 	error_log($msg . "\n", 3, get_log_file('formr_error.log'));
 }
 
-function opencpu_log_warning($msg) {// shorthand
-	error_log(date('Y-m-d H:i:s') . ' ' . $msg . "\n", 3, get_log_file('opencpu_warning.log'));
-}
-
-function opencpu_log($msg) {// shorthand
-	error_log(date('Y-m-d H:i:s') . ' ' . $msg . "\n", 3, get_log_file('opencpu_error.log'));
-}
-
 function get_log_file($filename) {
 	return INCLUDE_ROOT . "tmp/logs/$filename";
 }
