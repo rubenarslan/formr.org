c4de3953eafc53f4cfbf71190212e3c1479a7c12
Ruben C. Arslan <rubenarslan@gmail.com>
2015-07-04 15:49:23 +0200

    email channel for those wo token

--- View/public/register.php
+++ View/public/register.php
@@ -37,7 +37,19 @@
 					</div>
 				</div>
 			</div>
-			<p>If you don't have a referral token, you will need to write us an email to ask for an admin account.</p>
+			<p>If you don't have a referral token, you will need to write us an <a title=" We're excited to have people try this out, so you'll get a test account, if you're human or at least cetacean. But let us know a little about what you plan to do." class="schmail" href="mailto:IMNOTSENDINGSPAMTOruben.arslan@that-big-googly-eyed-email-provider.com?subject=<?=rawurlencode("formr private beta");?>&amp;body=<?=rawurlencode("If you are not a robot, I have high hopes that you can figure out how to get my proper email address from the above.Hi there!
+
+I'd like an admin account on formr. I'm totally not a robot.
+
+I already have registered with the email address from which I'm sending this request. 
+
+I'm affiliated with the University of Atlantis
+
+This is what I want to use formr for:
+[x] find out more about land mammals
+[x] plan cetacean world domination 
+[ ] excessively use your server resources
+");?>">email</a> to ask for an admin account. If you have one, you'll get your admin account once you confirm your email address.</p>
 			
 			<div class="form-group small-left">
 				<div class="controls">
