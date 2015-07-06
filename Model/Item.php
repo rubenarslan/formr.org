d73d87f387b28e8f95b53cb7ccd64bea1e69fec7
Ruben C. Arslan <rubenarslan@gmail.com>
2015-07-06 16:12:15 +0200

    call proper method

--- Model/Item.php
+++ Model/Item.php
@@ -229,11 +229,6 @@ class Item extends HTML_element {
 			$this->js_showif = preg_replace("/\s*\%contains\%\s*([a-zA-Z0-9_'\"]+)/", ".indexOf($1) > -1", $this->js_showif);
 			$this->js_showif = preg_replace("/\s*stringr::str_length\(([a-zA-Z0-9_'\"]+)\)/", "$1.length", $this->js_showif);
 
-			// For js_only specific show-ifs elements might be rendered but should be hidden
-			if (strpos($this->showif, '//js_only') !== false) {
-				$this->hidden = true;
-				$this->probably_render = true;
-			}
 		endif;
 	}
 
@@ -477,6 +472,8 @@ class Item extends HTML_element {
 	public function getShowIf() {
 		if (strstr($this->showif, "//js_only") === false) {
 			return $this->showif;
+		} else { // For js_only specific show-ifs elements might be rendered but should be hidden
+			$this->setVisibility(null);
 		}
 	}
 
