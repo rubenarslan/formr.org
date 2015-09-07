74aabf0a04c26060ec05808e9ee2a59dd9eb0dc7
Cyril Tata <cyril.tata@gmail.com>
2015-09-07 14:08:42 +0200

    api doc

--- application/View/public/documentation/api.php
+++ application/View/public/documentation/api.php
@@ -1,87 +1,97 @@
 <h3>API</h3>
 
+<p>
 The formr API is the primary way to get data/results out of the platform. It's a low-level HTTP-based API that you can use principally to get results of a 
 study for specified participants (sessions)
-
+</p>
+<p>
 Resource requests to the formr API require that you have a valid access token which you can obtain by providing api credentials (a client id and a client 
 secret)
+</p>
 
-API base URL: https://formr.org/api or https://api.formr.org
+<p>
+API base URL: <br />
+<code class="php">https://formr.org/api or https://api.formr.org</code>
+</p>
 
 
-#Obtaining Client ID and Client Secret.
---------------------------------------
-
+<h4>Obtaining Client ID and Client Secret</h4>
+<p>
 Access to the API is heavily restricted due to privacy issues so only the administrators of formr are able to provide api credentials to formr users. To 
 obtain api credentials send an email to rubenarslan@gmail.com or cyril.tata@gmail.com and your credentials will be sent to you in a few minutes.
+</p>
 
-#Obtaining An Access Token
--------------------------
-
+<h4>Obtaining An Access Token</h4>
+<p>
 An access token is an opaque string that identifies a formr user and can be used to make API calls without further authentication. formr api access tokens 
 are short-lived tokens and have a life span of about an hour.
+</p>
+	
+<p>To generate an access token you need to make an HTTP POST request to the token endpoint of the API</p>
 
-To generate an access token you need to make an HTTP POST request to the token endpoint of the API
-
-<code>
-
+<pre>
+<code class="php">
 POST /oauth/access_token?
      client_id={client-id}
     &client_secret={client-secret}
     &grant_type=client_credentials
-
 </code>
+</pre>
 
-This call will return a JSON object containing an access token which can be used to access the api without further authentication requred.
-
+<p>
+This call will return a JSON object containing an access token which can be used to access the api without further authentication required.
+<br />
 Sample successful response
+</p>
 
-<code>
+<pre>
+<code class="php">
 {
-	"access_token":"77c48497f09e95c504613635f0dc13d563504b4d",
+	"access_token":"XXXXXX3635f0dc13d563504b4d",
 	"expires_in":3600,
 	"token_type":"Bearer",
 	"scope":null
 }
 </code>
+</pre>
 
-The attribute 'expires_in' indicates the number of seconds for which is token is valid from its time of creation. If there is an error for example if the 
+The attribute <code>expires_in</code> indicates the number of seconds for which is token is valid from its time of creation. If there is an error for example if the 
 client details are not correct, then an error object is returned containing an error_description and sometimes an error_uri where you can read more about the 
 generated error. An example of an error object is
 
-<code>
+<pre>
+<code class="php">
 {
 	"error":"invalid_client",
 	"error_description":"The client credentials are invalid"
 }
 </code>
+</pre>
 
 
-#Making Resource Requests using generated access token.
------------------------------------------------------
+<h4>Making Resource Requests using generated access token</h4>
 
 With the generated access token, you are able to make requests to the resource endpoints of the formr API. For now only the results resource endpoint has 
 been implement
 
-##Getting study results over the API
-----------------------------------
+<h5>Getting study results over the API</h5>
 
-### Request
------------
+<h6> REQUEST </h6>
 To obtain the results of a particular session in a particular run, send a GET HTTP request to the get endpoint along side the access_token obtained above 
 and a request object. A request object in this case MUST be a JSON formatted string and passed using a parameter name called "request". For example
 
-<code>
-
+<pre>
+<code class="php">
 GET /get/results?
      access_token={access-token}
     &request={json-string-representing-request}
-
 </code>
+</pre>
 
-The JSON string representing the "request" object for the GET request has to be of the following format
+<p>The JSON string representing the "request" object for the GET request has to be of the following format</p>
 
-<code>
+<pre>
+<code class="php">
 request = {
 	run: {
 		name:  "run name",
@@ -97,49 +107,61 @@ request = {
 	}
 }
 </code>
+</pre>
 
-Parameters of the run object:
+<p>
+Parameters of the run object: <br /><br />
 
-name (required): This should be name name of the run as shown on formr.org
+<ul>
+	<li><b>name (required):</b> This should be name name of the run as shown on formr.org</li>
 
-sessions (required): An array of strings representing the sessions of interest whose results will be returned. At the moment the API does not support 
+	<li><b>sessions (required):</b> An array of strings representing the sessions of interest whose results will be returned. At the moment the API does not support 
 returning the results of all the sessions in the run in one request but this might be implemented in the future. A single session can also be specified 
-with the "session" parameter which MUST be a string an not an array of strings.
+with the <code>session</code> parameter which MUST be a string an not an array of strings.</li>
 
-surveys (optional): An array of objects. Each object represents a survey in the run that will be included in the results set. If no survey(s) is(are) 
-specified, all the surveys in the run will be included in the results set. A single survey can be specified by using the parameter name "survey" which must be an object and not an array of one object. Each survey object can have the following attributes:
-	name: The name of the survey as represented in formr.org
-	items: An array of items in the survey that should be included in the results set. If this is not specified, all the items in the survey will be 
-included in the results set which is generally not recommended.
+	<li><b>surveys (optional):</b> An array of objects. Each object represents a survey in the run that will be included in the results set. If no survey(s) is(are) 
+		specified, all the surveys in the run will be included in the results set. A single survey can be specified by using the parameter name <code>survey</code> which must be an object and not an array of one object. <br />
+		Each survey object can have the following attributes:
+		<ul>
+			<li><b>name:</b> The name of the survey as represented in formr.org</li>
+			<li><b>items:</b> An array of items in the survey that should be included in the results set. If this is not specified, all the items in the survey will be 
+included in the results set which is generally not recommended.</li>
+		</ul>
+		
+	</li>
 
-###Response
------------
+</ul>
+</p>
+
+<h6>RESPONSE</h6>
+
+<p>
 The response to a results request is a JSON object. The keys of this JSON structure are the names of the survey that were indicated in the requested and the value associated to each survey entry is an array of objects representing the results collected for that survey for all the requested sessions. An example of a response object could be the following:
+</p>
 
-<code>
-
+<pre>
+<code class="php">
 {
 	"survey_1": [{
 		"session": "sdaswew434df",
 		"survey_1_item_1": "answer1",
 		"survey_1_item_2": "answer2",
-		"survey_1_item_2": "answer3,
+		"survey_1_item_2": "answer3",
 	},
 	{
 		"session": "fdgdfg4323",
 		"survey_1_item_1": "answer4",
 		"survey_1_item_2": "answer5",
-		"survey_1_item_2": "answer6,
+		"survey_1_item_2": "answer6",
 	}],
 	"survey_2": [........]
 }
-
 </code>
+</pre>
 
+<h4>Using the formr API in R</h4>
 
-#Using the formr API in R
---------------------------
-
+<pre>
 // some note on the R package and how to include the library to the R work space
 
 ## Connecting to the API with client ID and client secret
@@ -149,6 +171,7 @@ The response to a results request is a JSON object. The keys of this JSON struct
 ## Getting results over the API
 
 // Code showing call to result helper funciton and parameter
+</pre>
 
 
 
