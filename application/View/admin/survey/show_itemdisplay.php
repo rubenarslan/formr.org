2e540ef161ce34fe228d09a9cdf80fc0e2c13e4d
Cyril Tata <cyril.tata@gmail.com>
2016-01-28 15:16:55 +0100

    use helper function for url

--- application/View/admin/survey/show_itemdisplay.php
+++ application/View/admin/survey/show_itemdisplay.php
@@ -16,32 +16,32 @@ Template::load('acp_nav');
 	<div class="list-group col-md-7">
 		<h5>Export as</h5>
 		<div class="list-group-item">
-			<h4 class="list-group-item-heading"><a href="<?= WEBROOT ?>admin/survey/<?= $study->name ?>/export_itemdisplay?format=csv"><i class="fa fa-floppy-o fa-fw"></i> CSV</a></h4>
+			<h4 class="list-group-item-heading"><a href="<?= admin_study_url($study->name, 'export_itemdisplay?format=csv')?>"><i class="fa fa-floppy-o fa-fw"></i> CSV</a></h4>
 			<p class="list-group-item-text">good for big files, problematic to import into German Excel (comma-separated)</p>
 		</div>
 
 		<div class="list-group-item">
-			<h4 class="list-group-item-heading"><a href="<?= WEBROOT ?>admin/survey/<?= $study->name ?>/export_itemdisplay?format=csv_german"><i class="fa fa-floppy-o fa-fw"></i> German CSV</a></h4>
+			<h4 class="list-group-item-heading"><a href="<?= admin_study_url($study->name, 'export_itemdisplay?format=csv_german')?>"><i class="fa fa-floppy-o fa-fw"></i> German CSV</a></h4>
 			<p class="list-group-item-text">good for German Excel (semicolon-separated)</p>
 		</div>
 
 		<div class="list-group-item">
-			<h4 class="list-group-item-heading"><a href="<?= WEBROOT ?>admin/survey/<?= $study->name ?>/export_itemdisplay?format=tsv"><i class="fa fa-floppy-o fa-fw"></i> TSV</a></h4>
+			<h4 class="list-group-item-heading"><a href="<?= admin_study_url($study->name, 'export_itemdisplay?format=tsv')?>"><i class="fa fa-floppy-o fa-fw"></i> TSV</a></h4>
 			<p class="list-group-item-text">tab-separated, human readable as plaintext</p>
 		</div>
 
 		<div class="list-group-item">
-			<h4 class="list-group-item-heading"><a href="<?= WEBROOT ?>admin/survey/<?= $study->name ?>/export_itemdisplay?format=xls"><i class="fa fa-floppy-o fa-fw"></i> XLS</a></h4>
+			<h4 class="list-group-item-heading"><a href="<?= admin_study_url($study->name, 'export_itemdisplay?format=xls')?>"><i class="fa fa-floppy-o fa-fw"></i> XLS</a></h4>
 			<p class="list-group-item-text">old excel format, won't work with more than 16384 rows or 256 columns</p>
 		</div>
 
 		<div class="list-group-item">
-			<h4 class="list-group-item-heading"><a href="<?= WEBROOT ?>admin/survey/<?= $study->name ?>/export_itemdisplay?format=xlsx"><i class="fa fa-floppy-o fa-fw"></i> XLSX</a></h4>
+			<h4 class="list-group-item-heading"><a href="<?= admin_study_url($study->name, 'export_itemdisplay?format=xlsx')?>"><i class="fa fa-floppy-o fa-fw"></i> XLSX</a></h4>
 			<p class="list-group-item-text">new excel format, higher limits</p>
 		</div>
 
 		<div class="list-group-item">
-			<h4 class="list-group-item-heading"><a href="<?= WEBROOT ?>admin/survey/<?= $study->name ?>/export_itemdisplay?format=json"><i class="fa fa-floppy-o fa-fw"></i> JSON</a></h4>
+			<h4 class="list-group-item-heading"><a href="<?= admin_study_url($study->name, 'export_itemdisplay?format=json')?>"><i class="fa fa-floppy-o fa-fw"></i> JSON</a></h4>
 			<p class="list-group-item-text">not particularly human-readable, but machines love it. This is probably the fastest way to get your data into R, just use <pre><code class="r hljs">data = as.data.frame(jsonlite::fromJSON("/path/to/exported_file.json"))</code></pre></p>
 		</div>
 	</div>
