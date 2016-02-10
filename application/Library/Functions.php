5a4c461419300eca253d845763810af35209a737
Cyril Tata <cyril.tata@gmail.com>
2016-02-10 16:10:35 +0100

    fill empty strings on parse error

--- application/Library/Functions.php
+++ application/Library/Functions.php
@@ -1105,7 +1105,7 @@ function opencpu_multistring_parse(Survey $survey, array $string_templates) {
 		return opencpu_string_key_parsing($strings);
 	} else {
 		notify_user_error(opencpu_debug($session), "There was a problem dynamically knitting something to HTML using openCPU.");
-		return opencpu_string_key_parsing($string_templates);
+		return fill_array(opencpu_string_key_parsing($string_templates));
 	}
 }
 
@@ -1323,4 +1323,11 @@ function strt_replace($str, $params) {
 		$str = str_replace('{'.$key.'}', $value, $str);
 	}
 	return $str;
+}
+
+function fill_array($array, $value = '') {
+	foreach ($array as $key => $v) {
+		$array[$key] = $value;
+	}
+	return $array;
 }
