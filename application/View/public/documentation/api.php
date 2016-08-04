d22ab6f5d9e195fed30358df7cb2c2e4cdb2906a
Cyril Tata <cyril.tata@gmail.com>
2016-08-04 20:46:15 +0200

    edit api documentation

--- application/View/public/documentation/api.php
+++ application/View/public/documentation/api.php
@@ -77,61 +77,28 @@ been implemented
 <h5>Getting study results over the API</h5>
 
 <h6> REQUEST </h6>
-To obtain the results of a particular session in a particular run, send a GET HTTP request to the get endpoint along side the access_token obtained above 
-and a request object. A request object in this case MUST be a JSON formatted string and passed using a parameter name called "request". For example
+To obtain the results of a particular set of sessions in a particular run, send a GET HTTP request to the get endpoint along side the access_token obtained above 
+together with the necessary parameters as shown below:
 
 <pre>
 <code class="http">
 GET /get/results?
      access_token={access-token}
-    &amp;request={json-string-representing-request}
-</code>
-</pre>
-
-<p>The JSON string representing the "request" object for the GET request has to be of the following format</p>
-
-<pre>
-<code class="json">
-{
-	"run": {
-		"name":  "run name",
-		"sessions": ["sdaswew434df", "fdgdfg4323"],
-		"surveys": [{
-			"name": "survey_1",
-			"items": ["survey_1_item_1", "survey_1_item_2", "survey_1_item_3"]
-		},
-		{
-			"name": "survey_2",
-			"items": ["survey_2_item_1", "survey_2_item_2", "survey_2_item_3"]
-		}]
-	}
-}
+    &amp;run[name]={name of the run as it appears on formr}
+    &amp;run[sessions]={comma separated list of session codes OR leave empty to get all sessions}
+    &amp;surveys[survey_name1]={comma separated list items to get from survey_name1 OR leave empty to get all items}
+    &amp;surveys[survey_name2]={comma separated list items to get from survey_name2 OR leave empty to get all items}
 </code>
 </pre>
 
 <p>
-Parameters of the run object:
+	<b><i>Notes:</i></b><br />
+	<ul>
+		<li><i>survey_name1</i> and <i>survey_name2</i> should be the actual survey names</li>
+		<li>If you want to get results for all surveys in the run you can omit the <i><b>survey</b></i> parameter</li>
+		<li>If you want to get all items from a survey, keep the items list empty.</li>
+	</ul>
 </p>
-
-<ul>
-	<li><b>name (required):</b> This should be name name of the run as shown on formr.org</li>
-
-	<li><b>sessions (required):</b> An array of strings representing the sessions of interest whose results will be returned. At the moment, the API does not support 
-returning the results of all the sessions in the run in one request but this might be implemented in the future. A single session can also be specified 
-with the <code>session</code> parameter which MUST be a string an not an array of strings.</li>
-
-	<li><b>surveys (optional):</b> An array of objects. Each object represents a survey in the run that will be included in the results set. If no survey(s) is(are) 
-		specified, all the surveys in the run will be included in the results set. A single survey can be specified by using the parameter name <code>survey</code> which must be an object and not an array of one object. <br />
-		Each survey object can have the following attributes:
-		<ul>
-			<li><b>name:</b> The name of the survey as represented in formr.org</li>
-			<li><b>items:</b> An array of items in the survey that should be included in the results set. If this is not specified, all the items in the survey will be 
-included in the results set which is generally not recommended.</li>
-		</ul>
-		
-	</li>
-
-</ul>
 
 <h6>RESPONSE</h6>
 
@@ -171,16 +138,14 @@ formr_api_access_token(client_id = "your_id", client_secret = "your_secret" )
 # To get the results row for a specific user, do the following
 results = formr_api_results(list(
 	run = list(
-	   name = "rotation",  # for which run do you want results
-	   session = "joyousCoyoteXXXLk5ByctNPryS4k-5JqZJYE19HwFhPu4FFk8beIHoBtyWniv46", # and for which user
-	   surveys = list(
-	   	list(
-	   	name = "rotation_exercise", # for which survey
-	   	items = list("exercise_1", "exercise_2") # and for which items
-	   	)
-	   )
-	 )
+		name = "rotation",  # for which run do you want results
+		sessions = c("joyousCoyoteXXXLk5ByctNPryS4k-5JqZJYE19HwFhPu4FFk8beIHoBtyWniv46") # and for which user
 	)
+	surveys = list(
+		rotation_exercise = c("exercise_1", "exercise_2"),
+		rotation_exercise2 = c("exercise2_1", "exercise2_2"),
+	)
+))
 # Now you can e.g. do:
 rotex = results$rotation_exercise
 rotex[, c("exercise_1","exercise_2")]
