b3fae22cdf59eecaf8e3b0f6ee632c282954bf73
Cyril Tata <cyril.tata@gmail.com>
2017-05-11 11:21:24 +0200

    add token check to newsletter subscription

--- application/View/public/newsletter.php
+++ application/View/public/newsletter.php
@@ -5,6 +5,7 @@
 		<h3 class="animate-box"><label for="email"><i class="fa fa-send"></i> Subscribe to our newsletter</label></h3>
 		<form action="<?php echo site_url('newsletter-subscription'); ?>" method="post" class="animate-box">
 			<input type="email" class="form-control" placeholder="Enter your email" id="email" name="n_email" style="height: 54px;">
+			<input type="hidden" name="<?php echo Session::REQUEST_TOKENS; ?>" value="<?php echo h(Session::getRequestToken()); ?>" />
 			<input type="submit" value="Send" class="btn btn-primary">
 		</form>
 
