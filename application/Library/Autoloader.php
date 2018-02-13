a51069659a37af01a4cb5129e017dcd6095a08f1
Cyril Tata <cyril.tata@gmail.com>
2018-02-13 10:46:36 +0100

    get rid of class map and add custom file finder in autoloader

--- application/Library/Autoloader.php
+++ application/Library/Autoloader.php
@@ -7,123 +7,7 @@
  */
 class Autoload {
 
-	private $classMap = array();
 	private static $loader = null;
-
-	public static function classMap() {
-		if (!defined('APPLICATION_ROOT')) {
-			define('APPLICATION_ROOT', realpath(dirname(__FILE__) . '../../') . '/');
-		}
-
-		if (!defined('APPLICATION_PATH')) {
-			define('APPLICATION_PATH', APPLICATION_ROOT . 'application/');
-		}
-
-		return array(
-			'AdminAjaxController' => APPLICATION_PATH . 'Controller/AdminAjaxController.php',
-			'AdminController' => APPLICATION_PATH . 'Controller/AdminController.php',
-			'AdminMailController' => APPLICATION_PATH . 'Controller/AdminMailController.php',
-			'AdminRunController' => APPLICATION_PATH . 'Controller/AdminRunController.php',
-			'AdminSurveyController' => APPLICATION_PATH . 'Controller/AdminSurveyController.php',
-			'AnimalName' => APPLICATION_PATH . 'Library/AnimalName.php',
-			'ApiController' => APPLICATION_PATH . 'Controller/ApiController.php',
-			'ApiHelper' => APPLICATION_PATH . 'Helper/ApiHelper.php',
-			'Branch' => APPLICATION_PATH . 'Model/Branch.php',
-			'Cache' => APPLICATION_PATH . 'Library/Cache.php',
-			'Config' => APPLICATION_PATH . 'Library/Config.php',
-			'Controller' => APPLICATION_PATH . 'Controller/Controller.php',
-			'Crypto' => APPLICATION_PATH . 'Library/Crypto.php',
-			'CURL' => APPLICATION_PATH . 'Library/CURL.php',
-			'DB' => APPLICATION_PATH . 'Library/DB.php',
-			'DB_Select' => APPLICATION_PATH . 'Model/DB.php',
-			'Deamon' => APPLICATION_PATH . 'Library/Deamon.php',
-			'Email' => APPLICATION_PATH . 'Model/Email.php',
-			'EmailAccount' => APPLICATION_PATH . 'Model/EmailAccount.php',
-			'EmailQueue' => APPLICATION_PATH . 'Library/EmailQueue.php',
-			'External' => APPLICATION_PATH . 'Model/External.php',
-			'GearmanWorkerHelper' =>  APPLICATION_PATH . 'Helper/GearmanWorkerHelper.php',
-			'HTML_element' => APPLICATION_PATH . 'Model/Item.php',
-			'Item' => APPLICATION_PATH . 'Model/Item.php',
-			'ItemFactory' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_blank' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_calculate' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_cc' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_check' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_check_button' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_choose_two_weekdays' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_color' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_date' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_datetime' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_datetime_local' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_email' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_file' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_geopoint' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_get' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_image' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_ip' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_letters' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_mc' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_mc_button' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_mc_heading' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_mc_multiple' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_mc_multiple_button' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_month' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_note' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_number' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_random' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_range' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_range_ticks' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_rating_button' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_referrer' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_select_multiple' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_select_one' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_select_or_add_multiple' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_select_or_add_one' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_server' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_sex' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_submit' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_tel' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_text' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_textarea' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_time' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_timezone' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_url' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_week' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_year' => APPLICATION_PATH . 'Model/Item.php',
-			'Item_yearmonth' => APPLICATION_PATH . 'Model/Item.php',
-			'LogParser' => APPLICATION_PATH . 'Library/LogParser.php',
-			'OAuthHelper' => APPLICATION_PATH . 'Helper/OAuthHelper.php',
-			'OpenCPU' => APPLICATION_PATH . 'Library/OpenCPU.php',
-			'OSF' => APPLICATION_PATH . 'Library/OSF.php',
-			'Page' => APPLICATION_PATH . 'Model/Page.php',
-			'Pagination' => APPLICATION_PATH . 'Model/Pagination.php',
-			'Pause' => APPLICATION_PATH . 'Model/Pause.php',
-			'PublicController' => APPLICATION_PATH . 'Controller/PublicController.php',
-			'Request' => APPLICATION_PATH . 'Library/Request.php',
-			'Response' => APPLICATION_PATH . 'Library/Response.php',
-			'Router' => APPLICATION_PATH . 'Library/Router.php',
-			'Run' => APPLICATION_PATH . 'Model/Run.php',
-			'RunController' => APPLICATION_PATH . 'Controller/RunController.php',
-			'RunHelper' => APPLICATION_PATH . 'Helper/RunHelper.php',
-			'RunSession' => APPLICATION_PATH . 'Model/RunSession.php',
-			'RunSessionWorkerHelper' => APPLICATION_PATH . 'Helper/GearmanWorkerHelper.php',
-			'RunUnit' => APPLICATION_PATH . 'Model/RunUnit.php',
-			'RunUnitFactory' => APPLICATION_PATH . 'Model/RunUnit.php',
-			'RunWorkerHelper' => APPLICATION_PATH . 'Helper/GearmanWorkerHelper.php',
-			'Session' => APPLICATION_PATH . 'Library/Session.php',
-			'Shuffle' => APPLICATION_PATH . 'Model/Shuffle.php',
-			'Site' => APPLICATION_PATH . 'Model/Site.php',
-			'SkipBackward' => APPLICATION_PATH . 'Model/SkipBackward.php',
-			'SkipForward' => APPLICATION_PATH . 'Model/SkipForward.php',
-			'SpreadsheetReader' => APPLICATION_PATH . 'Model/SpreadsheetReader.php',
-			'SuperadminController' => APPLICATION_PATH . 'Controller/SuperadminController.php',
-			'Survey' => APPLICATION_PATH . 'Model/Survey.php',
-			'Template' => APPLICATION_PATH . 'Library/Template.php',
-			'UnitSession' => APPLICATION_PATH . 'Model/UnitSession.php',
-			'User' => APPLICATION_PATH . 'Model/User.php',
-		);
-	}
-
 	/**
 	 * Register this class instance as an autoloader
 	 * @throws RuntimeException
@@ -171,44 +55,41 @@ class Autoload {
 	 * @return string|null
 	 */
 	private function findFile($class) {
-		if (isset($this->classMap[$class])) {
-			return $this->classMap[$class];
+		if (!defined('APPLICATION_ROOT')) {
+			define('APPLICATION_ROOT', realpath(dirname(__FILE__) . '../../') . '/');
+		}
+
+		if (!defined('APPLICATION_PATH')) {
+			define('APPLICATION_PATH', APPLICATION_ROOT . 'application/');
+		}
+
+		if (strpos($class, 'Controller') !== false) {
+			$file = APPLICATION_PATH . "Controller/{$class}.php";
+		} elseif (strpos($class, 'Helper') !== false) {
+			$file = APPLICATION_PATH . "Helper/{$class}.php";
+		} else {
+			$class = str_replace('Factory', '', $class);
+			$libraryPath = APPLICATION_PATH . "Library/{$class}.php";
+			$modelPath = APPLICATION_PATH . "Model/{$class}.php";
+			if (strpos($class, 'Item_') === 0 || strpos($class, 'HTML_') === 0) {
+				$file = APPLICATION_PATH . 'Model/Item.php';
+			} elseif (file_exists($modelPath)) {
+				$file = $modelPath;
+			} elseif (file_exists($libraryPath)) {
+				$file = $libraryPath;
+			}
+		}
+
+		if (!empty($file)) {
+			return $file;
 		}
 		return false;
-	}
-
-	/**
-	 * Set class map for autoloader
-	 *
-	 * @param array $map
-	 */
-	public function setClassMap(array $map) {
-		$this->classMap = array_merge($this->classMap, $map);
-	}
-
-	/**
-	 * Get autoloader class map
-	 * @return array
-	 */
-	public function getClassMap() {
-		return $this->classMap;
-	}
-
-	/**
-	 * Add/override a class path
-	 *
-	 * @param string $class
-	 * @param string $path
-	 */
-	public function addClass($class, $path) {
-		$this->classMap[$class] = $path;
 	}
 
 	public static function getLoader() {
 		if (self::$loader === null) {
 			/* @var $loader Autoload */
 			$loader = new self();
-			$loader->setClassMap(self::classMap());
 			$loader->register();
 			self::$loader = $loader;
 		}
