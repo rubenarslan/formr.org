5dad92fd9a75219d3c259cc3994cfd0c6309eae4
Cyril Tata <cyril.tata@gmail.com>
2018-02-16 18:18:43 +0100

    Render input with right Y-m value

--- application/Model/Item/Yearmonth.php
+++ application/Model/Item/Yearmonth.php
@@ -9,5 +9,15 @@ class Yearmonth_Item extends Datetime_Item {
 	protected $prepend = 'fa-calendar-o';
 	protected $html5_date_format = 'Y-m-01';
 
+	protected function render_input() {
+		if ($this->value_validated !== null) {
+			$parts = explode('-', $this->value_validated);
+			array_pop($parts);
+			$this->input_attributes['value'] = implode('-', $parts);
+		}
+
+		return sprintf('<span><input %s /></span>', self::_parseAttributes($this->input_attributes));
+	}
+
 }
 
