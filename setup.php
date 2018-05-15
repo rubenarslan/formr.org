c8363913a8e197ed9c411a3f52195e6700b90956
Cyril Tata <cyril.tata@gmail.com>
2018-05-15 09:04:20 +0200

    version bump

--- setup.php
+++ setup.php
@@ -35,7 +35,7 @@ if (php_sapi_name() != 'cli') {
 }
 
 // Set current formr version (bumped on release)
-$settings['version'] = 'v0.17.10';
+$settings['version'] = 'v0.17.11';
 
 // Load application autoloader
 $autoloader = require_once APPLICATION_PATH . 'Library/Autoloader.php';
