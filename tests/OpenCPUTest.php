c50e86468725278549633e49767cc1fd4a4b9ab9
Matthias P. Walther <m.walther@wwu.de>
2018-07-25 10:44:32 +0200

    Ported tests to PHPUnit 6+, removed unneeded code, fixed dependencies, added phpunit.xml

--- tests/OpenCPUTest.php
+++ tests/OpenCPUTest.php
@@ -1,31 +1,27 @@
 <?php
+
+require_once '../application/Library/Config.php';
+require_once '../application/Library/CURL.php';
+require_once '../application/Library/OpenCPU.php';
+
 /* 
  * Test Config class
  */
+class OpenCPUTest extends PHPUnit\Framework\TestCase {
 
-#phpunit --bootstrap ../Library/Config.php ../Library/OpenCPU.php OpenCPUTest
-#
-// you can use phpunit bootstrap for this but fuck it
-require_once '../Library/Functions.php';
-require_once '../Library/Config.php';
-require_once '../Library/CURL.php';
-require_once '../Library/OpenCPU.php';
+	public function configProvider() {
+		$settings = array(
+			'opencpu_instance' => 'http://opencpu.psych.bio.uni-goettingen.de',
+		);
+		return array(array($settings));
+	}
 
-class OpenCPUTest extends PHPUnit_Framework_TestCase {
-
-    public function configProvider() {
-        $settings = array(
-            'opencpu_instance' => 'http://opencpu.psych.bio.uni-goettingen.de',
-        );
-        return array(array($settings));
-    }
-
-    /**
-     *
-     * @dataProvider configProvider
-     */
-    public function testConnection($settings) {
-        Config::initialize($settings);
+	/**
+	 *
+	 * @dataProvider configProvider
+	 */
+	public function testConnection($settings) {
+		Config::initialize($settings);
 		$ocpu = OpenCPU::getInstance('opencpu_instance');
 		$session = $ocpu->snippet('rnorm(5)');
 		$this->assertInstanceOf('OpenCPU_Session', $session);
@@ -38,12 +34,10 @@ class OpenCPUTest extends PHPUnit_Framework_TestCase {
 		$this->write($session->getStdout());
 		$this->write("Object");
 		$this->write($session->getObject());
-    }
+	}
 
 	private function write($object) {
 		echo "\n";
 		print_r($object);
 	}
-
 }
-
