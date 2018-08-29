1877c6bf29c2f9c2409a879a0c99f49c1a2f76a3
Cyril Tata <cyril.tata@gmail.com>
2018-08-29 14:21:34 +0200

    move checkbox selection js to admin/main.js

--- webroot/assets/admin/js/main.js
+++ webroot/assets/admin/js/main.js
@@ -763,3 +763,23 @@ function _init() {
     });
   };
 }(jQuery));
+
+/*
+ * Admin > Run
+ * -----------------------
+ *
+ */
+jQuery(document).ready(function() {
+	// Select all checkbox in 'Users overview'
+	$('#user-overview-select-all').click(function() {
+		var $select = $(this);
+		var $checkboxes = $select.parents('table').find('.ba-select-session');
+
+		if ($select.is(':checked')) {
+			$checkboxes.prop('checked', true);
+		} else {
+			$checkboxes.prop('checked', false);
+		}
+	});
+
+});
