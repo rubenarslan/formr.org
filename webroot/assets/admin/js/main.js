10f9a9f9d3e06ef7a0ae6ea123ae8fcfcb678630
Matthias P. Walther <m.walther@wwu.de>
2018-08-30 09:34:42 +0200

    Move JS code to new admin.js
    Closes #379

--- webroot/assets/admin/js/main.js
+++ webroot/assets/admin/js/main.js
@@ -763,23 +763,3 @@ function _init() {
     });
   };
 }(jQuery));
-
-/*
- * Admin > Run
- * -----------------------
- *
- */
-jQuery(document).ready(function() {
-	// Select all checkbox in 'Users overview'
-	$('#user-overview-select-all').click(function() {
-		var $select = $(this);
-		var $checkboxes = $select.parents('table').find('.ba-select-session');
-
-		if ($select.is(':checked')) {
-			$checkboxes.prop('checked', true);
-		} else {
-			$checkboxes.prop('checked', false);
-		}
-	});
-
-});
