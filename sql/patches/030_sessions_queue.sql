25bb5950253cd3d9c298845ec7dc4e1d01678d2b
Cyril Tata <cyril.tata@gmail.com>
2019-03-27 12:33:36 +0100

    add created timestamp to queue item

--- sql/patches/030_sessions_queue.sql
+++ sql/patches/030_sessions_queue.sql
@@ -2,6 +2,7 @@ CREATE TABLE `survey_sessions_queue` (
   `unit_session_id` bigint(20) unsigned NOT NULL,
   `run_session_id` int(10) unsigned NOT NULL,
   `unit_id` int(10) unsigned NOT NULL,
+  `created` int(10) unsigned NOT NULL,
   `expires` int(10) unsigned NOT NULL,
   `run` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
   `counter` int(10) unsigned NOT NULL DEFAULT '0'
