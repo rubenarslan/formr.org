1c05f8ef7e9d0d2b58a8ee5278037ff7a3cb8cc0
Cyril Tata <cyril.tata@gmail.com>
2019-05-09 12:21:14 +0200

    fix OpenCPUTest unit test

--- tests/OpenCPUTest.php
+++ tests/OpenCPUTest.php
@@ -10,9 +10,12 @@ require_once '../application/Library/OpenCPU.php';
 class OpenCPUTest extends PHPUnit\Framework\TestCase {
 
 	public function configProvider() {
-		$settings = array(
-			'opencpu_instance' => 'http://opencpu.psych.bio.uni-goettingen.de',
-		);
+		$settings = array();
+        $settings['opencpu_instance'] = array(
+            'base_url' => 'http://opencpu.psych.bio.uni-goettingen.de',
+            'r_lib_path' => '/usr/local/lib/R/site-library'
+        );
+
 		return array(array($settings));
 	}
 
@@ -21,11 +24,13 @@ class OpenCPUTest extends PHPUnit\Framework\TestCase {
 	 * @dataProvider configProvider
 	 */
 	public function testConnection($settings) {
-		Config::initialize($settings);
+        Config::initialize($settings);
 		$ocpu = OpenCPU::getInstance('opencpu_instance');
-		$session = $ocpu->snippet('rnorm(5)');
+		$session = $ocpu->snippet($this->getCodeSnippet());
 		$this->assertInstanceOf('OpenCPU_Session', $session);
 
+        $this->write("Session");
+        $this->write($session->getResponseHeaders()['X-Ocpu-Session']);
 		$this->write("Files");
 		$this->write($session->getFiles());
 		$this->write("Console");
@@ -33,11 +38,18 @@ class OpenCPUTest extends PHPUnit\Framework\TestCase {
 		$this->write("Stdout");
 		$this->write($session->getStdout());
 		$this->write("Object");
-		$this->write($session->getObject());
+		$this->write($session->getObject('json'));
 	}
 
 	private function write($object) {
 		echo "\n";
 		print_r($object);
 	}
+
+    private function getCodeSnippet($snippet = 0) {
+        $snippets = array();
+        $snippets[] = "library(formr) \n .formr\$last_action_time = as.POSIXct('2019-05-09 17:00:35 CEST') \n ! time_passed(minutes = 10)";
+        $snippets[] = "rnorm(5)";
+        return $snippets[$snippet];
+    }
 }
