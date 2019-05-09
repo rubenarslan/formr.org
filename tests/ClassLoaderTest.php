1093d020f475ef4c3b3b1295a6873311190e378f
Cyril Tata <cyril.tata@gmail.com>
2019-05-09 19:01:39 +0200

    autoformat

--- tests/ClassLoaderTest.php
+++ tests/ClassLoaderTest.php
@@ -3,17 +3,18 @@
 require_once('../setup.php');
 require_once('../application/Library/DB.php');
 
-/** 
+/**
  * Test Autoloder class
  */
 class ClassLoaderTest extends PHPUnit\Framework\TestCase {
 
-	public function testClasses() {
-		$email = new Email(null);
-		$this->assertTrue(class_exists('DB') && class_exists('Email') && class_exists('RunUnit'), "Some class in test not autoloaded");
-	}
+    public function testClasses() {
+        $email = new Email(null);
+        $this->assertTrue(class_exists('DB') && class_exists('Email') && class_exists('RunUnit'), "Some class in test not autoloaded");
+    }
 
-	public function testNonExistent() {
-		$this->assertFalse(class_exists('User', false));
-	}
+    public function testNonExistent() {
+        $this->assertFalse(class_exists('User', false));
+    }
+
 }
--- tests/FirstTest.php
+++ tests/FirstTest.php
@@ -3,25 +3,27 @@
 require_once('../setup.php');
 
 class FirstTest extends PHPUnit\Framework\TestCase {
-	private function checkPageForPHPErrors($address) {
-		$bla = file_get_contents($address);
-		$this->assertEquals(FALSE, strpos($bla, "Fatal error") OR strpos($bla, "Parse error") OR strpos($bla, "<b>Warning</b>:") OR strpos($bla, "<b>Notice</b>:") );
-	}
 
-	public function testAllPages() {
-		$this->checkPageForPHPErrors(WEBROOT);
-		$this->checkPageForPHPErrors(WEBROOT."/public/login");
-		$this->checkPageForPHPErrors(WEBROOT."/public/documentation");
-		$this->checkPageForPHPErrors(WEBROOT."/public/studies");
-		$this->checkPageForPHPErrors(WEBROOT."/public/about");
-		$this->checkPageForPHPErrors(WEBROOT."/public/register");
-		$this->checkPageForPHPErrors(WEBROOT."/public/logout");
+    private function checkPageForPHPErrors($address) {
+        $bla = file_get_contents($address);
+        $this->assertEquals(FALSE, strpos($bla, "Fatal error") OR strpos($bla, "Parse error") OR strpos($bla, "<b>Warning</b>:") OR strpos($bla, "<b>Notice</b>:"));
+    }
 
-		$this->checkPageForPHPErrors(WEBROOT."/admin/mail/");
-		$this->checkPageForPHPErrors(WEBROOT."/admin/mail/edit");
+    public function testAllPages() {
+        $this->checkPageForPHPErrors(WEBROOT);
+        $this->checkPageForPHPErrors(WEBROOT . "/public/login");
+        $this->checkPageForPHPErrors(WEBROOT . "/public/documentation");
+        $this->checkPageForPHPErrors(WEBROOT . "/public/studies");
+        $this->checkPageForPHPErrors(WEBROOT . "/public/about");
+        $this->checkPageForPHPErrors(WEBROOT . "/public/register");
+        $this->checkPageForPHPErrors(WEBROOT . "/public/logout");
 
-		$this->checkPageForPHPErrors(WEBROOT."/admin/run/");
+        $this->checkPageForPHPErrors(WEBROOT . "/admin/mail/");
+        $this->checkPageForPHPErrors(WEBROOT . "/admin/mail/edit");
 
-		$this->checkPageForPHPErrors(WEBROOT."/admin/survey/");
-	}
+        $this->checkPageForPHPErrors(WEBROOT . "/admin/run/");
+
+        $this->checkPageForPHPErrors(WEBROOT . "/admin/survey/");
+    }
+
 }
