953197823e40f7eda5029bca9cc1fbaf94d113e1
Matthias P. Walther <m.walther@wwu.de>
2019-10-21 12:45:05 +0200

    Upate database structure for push_subscriptions table

--- sql/patches/031_push_subscriptions.sql
+++ sql/patches/031_push_subscriptions.sql
@@ -1,8 +1,11 @@
 CREATE TABLE `survey_push_subscriptions` (
-      `id` INT NOT NULL
-      `run_id` INT NOT NULL,
-      `endpoint` LONGTEXT NOT NULL,
-      `auth` VARCHAR(45) NOT NULL,
-      `p256dh` MEDIUMTEXT NULL,
-      `session` CHAR(64) NOT NULL,
-      PRIMARY KEY (`id`));
+      `run_id` int(11) NOT NULL,
+      `endpoint` varchar(500) NOT NULL,
+      `auth` varchar(45) NOT NULL,
+      `p256dh` mediumtext,
+      `session` char(64) NOT NULL,
+      `id` int(11) NOT NULL AUTO_INCREMENT,
+      PRIMARY KEY (`id`),
+      UNIQUE KEY `unique_index` (`run_id`,`endpoint`,`session`)
+      ) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8;
+
