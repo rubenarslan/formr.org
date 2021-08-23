0e08b0f1235d027e8191264693cbb682b2bf52db
Ruben C. Arslan <rubenarslan@gmail.com>
2021-08-23 17:53:08 +0200

    unstick skips

---
+++ sql/patches/036_unstick_branches.sql
@@ -0,0 +1,4 @@
+UPDATE `survey_unit_sessions`
+INNER JOIN `survey_branches` ON `survey_branches`.`id` = `survey_unit_sessions`.`unit_id`
+SET `survey_unit_sessions`.`queued` = 1
+WHERE `survey_branches`.`automatically_jump` = 1 AND `survey_branches`.`automatically_go_on` = 1 AND `survey_unit_sessions`.`ended` IS NULL AND `survey_unit_sessions`.`created` > '2021-08-01';
