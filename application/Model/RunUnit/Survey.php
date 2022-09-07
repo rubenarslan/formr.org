e0a95bfa6fadddceb0ae486e8810a02a1ffe0261
Cyril Tata <cyril.tata@gmail.com>
2022-09-07 12:44:51 +0200

    reset survey expiry once participant starts filling

--- application/Model/RunUnit/Survey.php
+++ application/Model/RunUnit/Survey.php
@@ -101,44 +101,61 @@ class Survey extends RunUnit {
         } else {
             $now = time();
 
-            $last_active = $this->getUnitSessionLastVisit($unitSession); // when was the user last active on the study
-            $expire_invitation_time = $expire_inactivity_time = 0; // default to 0 (means: other values supervene. users only get here if at least one value is nonzero)
+			// Expire if the user started filling the survey and then stopped
+			// This uses "Inactivity Expiration" but can be overriden by "Start editing within"
+            $last_active = $this->getUnitSessionLastVisit($unitSession);
+            $expires = $expire_invitation_time = $expire_inactivity_time = 0;
             if ($expire_inactivity !== 0 && $last_active != null && strtotime($last_active)) {
-                $expire_inactivity_time = strtotime($last_active) + ($expire_inactivity * 60);
+                $expires = strtotime($last_active) + ($expire_inactivity * 60);
             }
+			
+			// Get expiration time based on when invitation was sent or user arrived in unit but not reacted
+			// This uses "Start editing within"
             $invitation_sent = $unitSession->created;
             if ($expire_invitation !== 0 && $invitation_sent && strtotime($invitation_sent)) {
-                $expire_invitation_time = strtotime($invitation_sent) + ($expire_invitation * 60);
-                if ($grace_period !== 0 && $last_active) {
-                    $expire_invitation_time = $expire_invitation_time + ($grace_period * 60);
-                }
+                $expires = strtotime($invitation_sent) + ($expire_invitation * 60);
             }
 
-            $expire = max($expire_inactivity_time, $expire_invitation_time);
+			// Get expiration time if user already started filling and there is a maximum time to spend on the survey
+			// This uses "finishing editing within"
+			$first_active = $this->getUnitSessionFirstVisit($unitSession);
+			$first_submit = $this->getUnitSessionFirstVisit($unitSession, 'survey_items_display.saved != "' . $invitation_sent . '"');
+			if ($last_active && $last_active != $invitation_sent && 
+				$grace_period !== 0 && 
+				$first_active != null && strtotime($first_active) &&
+				$first_submit != null && strtotime($first_submit)	
+			) {
+				$expires = strtotime($first_submit) + ($grace_period * 60);
+            }
 
-            $data['expires'] = max(0, $expire_invitation_time);
+            $data['expires'] = max(0, $expires);
             $data['expired'] = ($data['expires'] > 0) && ($now > $data['expires']);
-            $data['queued'] = UnitSessionQueue::QUEUED_TO_EXECUTE;
+            $data['queued'] = UnitSessionQueue::QUEUED_TO_END;
 
             return $data;
         }
     }
 
-    public function getUnitSessionLastVisit(UnitSession $unitSession) {
+    public function getUnitSessionLastVisit(UnitSession $unitSession, $order = 'desc', $where = null) {
         // use created (item render time) if viewed time is lacking
-        $arr = $this->db->select(array('COALESCE(`survey_items_display`.shown,`survey_items_display`.created)' => 'last_viewed'))
+        $arr = $this->db->select(array('COALESCE(`survey_items_display`.saved,`survey_items_display`.created)' => 'last_viewed'))
                 ->from('survey_items_display')
                 ->leftJoin('survey_items', 'survey_items_display.session_id = :session_id', 'survey_items.id = survey_items_display.item_id')
                 ->where('survey_items_display.session_id IS NOT NULL')
                 ->where('survey_items.study_id = :study_id')
-                ->order('survey_items_display.shown', 'desc')
-                ->order('survey_items_display.created', 'desc')
+				->where($where ? $where : '1=1')
+                ->order('survey_items_display.saved', $order)
+                ->order('survey_items_display.created', $order)
                 ->limit(1)
-                ->bindParams(array('session_id' => $unitSession->id, 'study_id' => $this->id))
+                ->bindParams(array('session_id' => $unitSession->id, 'study_id' => $this->surveyStudy->id))
                 ->fetch();
 
         return isset($arr['last_viewed']) ? $arr['last_viewed'] : null;
     }
+	
+	function getUnitSessionFirstVisit(UnitSession $unitSession, $where = null) {
+		return $this->getUnitSessionLastVisit($unitSession, 'asc', $where);
+	}
 
     public function getUnitSessionOutput(UnitSession $unitSession) {
         try {
