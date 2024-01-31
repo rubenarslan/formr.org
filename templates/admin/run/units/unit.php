f9ded43512784ef36570ba0f3c2940ed8cc6f4c8
Jannis Kramer <jkramer2@uni-muenster.de>
2024-01-31 01:00:44 +0100

    feat: add confirmation on privacy unit removal

--- templates/admin/run/units/unit.php
+++ templates/admin/run/units/unit.php
@@ -4,7 +4,7 @@
     </div>
     <div class="col-xs-3 run_unit_position">
         <h1><i class="muted fa fa-2x <?= $unit->icon ?>"></i></h1>
-        <?= $unit->displayUnitSessionsCount() ?> <button href="ajax_remove_run_unit_from_run" class="remove_unit_from_run btn btn-xs hastooltip" title="Remove unit <?= $unit->id ?>/<?= $unit->run_unit_id ?> from run" type="button"><i class="fa fa-times"></i></button> <br />
+        <?= $unit->displayUnitSessionsCount() ?> <button href="ajax_remove_run_unit_from_run" class="remove_unit_from_run btn btn-xs hastooltip" title="Remove unit <?= $unit->id ?>/<?= $unit->run_unit_id ?> from run" type="button" <?php if(strtolower($unit->type) == 'privacy'): ?>onclick="if (!confirm('A privacy consent is required in some countries. Are you sure you want to delete this unit?')) { event.preventDefault(); event.stopPropagation(); event.stopImmediatePropagation(); }"<?php endif; ?>><i class="fa fa-times"></i></button> <br />
         <input class="position" value="<?= $unit->position ?>" type="number" name="position[<?= $unit->run_unit_id ?>]" step="1" max="32000" min="-32000"> <br />
     </div>
     <div class="col-xs-9 run_unit_dialog">
