395eebf313b44903ea5b1460cf486eaa82149124
Ruben C. Arslan <rubenarslan@gmail.com>
2024-06-03 15:50:37 +0200

    don't create more than 1 superadmin

--- bin/add_user.php
+++ bin/add_user.php
@@ -29,14 +29,14 @@ print("$users exist already");
 
 if($config['level'] > 1 && $users > 0) {
 	print("Cannot create superadmins when users already exist");
+} else {
+	$inserted = $db->insert('survey_users', array(
+		'email' => $config['email'],
+		'created' => mysql_now(),
+		'password' => $config['hash'],
+		'user_code' => crypto_token(48),
+		'referrer_code' => "created from host",
+		'email_verified' => 1,
+		'admin' => $config['level']
+	));
 }
-
-$inserted = $db->insert('survey_users', array(
-	'email' => $config['email'],
-	'created' => mysql_now(),
-	'password' => $config['hash'],
-	'user_code' => crypto_token(48),
-	'referrer_code' => "created from host",
-	'email_verified' => 1,
-	'admin' => $config['level']
-));
