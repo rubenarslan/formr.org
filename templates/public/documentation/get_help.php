6cb9537a17235982d765ff82d10cf58405061b49
Ruben C. Arslan <rubenarslan@gmail.com>
2024-12-16 18:16:44 +0100

    update places to find help

--- templates/public/documentation/get_help.php
+++ templates/public/documentation/get_help.php
@@ -2,7 +2,7 @@
 
 <h4>Where to get help</h4>
 <p>If you're a participant in one of the studies implemented in formr, please reach out to the person running the study.</p>
-<p>If you're running a study yourself, there's several places to look.<p>
+<p>If you're running a study yourself, there's several places to look for help.<p>
 <ul class="fa-ul-more-padding">
     <li>
         this documentation is a good start, just click on any of the tabs above.
@@ -25,9 +25,12 @@
         You'll find answers to some <a href="https://github.com/rubenarslan/formr.org/wiki/FAQ---frequently-asked-questions">frequently asked questions</a> there too.
     </li>
     <li>
-        You can <a href="https://groups.google.com/forum/#!forum/formr" title="you can ask and answer other admin users' questions here">ask and answer questions on our mailing list</a>.
+        You can ask (and answer!) questions in our <a href="https://github.com/rubenarslan/formr.org/discussions">Github Discussions forum</a> for community support. Previously, we used a mailing list, but spam made it unusable. You can still <a href="https://groups.google.com/forum/#!forum/formr"> browse it</a>.
     </li>
     <li>
         If you find a bug, <a href="https://github.com/rubenarslan/formr.org/issues">this is the place to describe it</a> (preferably in a way that allows us to reproduce it, but we're also accepting Yeti reports).
+    </li>
+    <li>
+        Please do not email the creators of formr for general support unless you've bought one of our consulting packages. Do reach out if you think sharing your concerns publicly could result in security concerns.
     </li>
 </ul>
