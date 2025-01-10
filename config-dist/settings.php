a3cf3f742385960560d5adb4ff662edf87d30e69
Ruben C. Arslan <rubenarslan@gmail.com>
2025-01-11 00:05:55 +0100

    unneeded second crypto key

--- config-dist/settings.php
+++ config-dist/settings.php
@@ -309,6 +309,5 @@ $settings['copy_context'] = array(
 $settings['2fa'] = array(
     'enabled' => true, // whether 2FA is enabled for the instance
     'required' => false, // whether 2FA is required for all users
-    'allow_during_signup' => false, // whether users can set up 2FA during signup
-    'encryption_key_file' => null, // path to encryption key file for TOTP secrets, defaults to {APPLICATION_ROOT}/formr-2fa.key
+    'allow_during_signup' => false
 );
